661d230c3510bdd0dc0026af9c6d60cc67808bc3
koz-y <77233172+koz-y@users.noreply.github.com>
2022-03-25 23:04:00 +0900

    Setup for Heroku

--- server/index.js
+++ server/index.js
@@ -1,24 +1,31 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const express = require('express')
 const mongoose = require('mongoose')
+const path = require('path')
 
 const FakeDb = require('./fake-db')
-const config = require('./config/dev')
+const config = require('./config/index')
 const productRouts = require('./routes/products')
 
 const app = express()
 const port = process.env.PORT || 3001
 
 mongoose.connect(config.DB_URI).then(() => {
-  const fakeDb = new FakeDb()
-  // fakeDb.seeDb()
-  fakeDb.initDb()
+  if (process.env.NODE_ENV !== 'production') {
+    const fakeDb = new FakeDb()
+    // fakeDb.initDb()
+  }
 })
 
-// app.get('/products', (req, res) => {
-//   res.json({ success: true })
-// })
 app.use('/api/v1/products', productRouts)
+
+if (process.env.NODE_ENV === 'production') {
+  const appPath = path.join(__dirname, '..', 'dist', 'my-elementary-app')
+  app.use(express.static(appPath))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(appPath, 'index.html'))
+  })
+}
 
 app.listen(port, () => {
   console.log(`Node Server is fine on port ${port}`)
